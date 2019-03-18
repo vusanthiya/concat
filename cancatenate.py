8f33cda4c3388cdc290429a2736b5cce9dbfee5a
vusanthiya <vusanthiya@gmail.com>
2019-03-18 11:17:25 +0530

    Update and rename README.md to cancatenate.py

---
+++ cancatenate.py
@@ -0,0 +1,19 @@
+void main()
+{
+char s1[100],s2[100];
+int i,j;
+printf("Enter the string1:");
+scanf("%s",s1);
+printf("Enter the string2:");
+scanf("%s",s2);
+for(i=0;s1[i]!='\0';++i)
+{
+for(j=0;s2[i]!='\0';++j;++i)
+{
+s1[i]=s2[i];
+}
+s1[i]!='\0';
+printf(s1);
+}
+}
+}
